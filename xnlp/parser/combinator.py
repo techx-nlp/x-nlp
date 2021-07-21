0d0bc6cb8f9ee8cc67ed80d66d3da3344feb5cbf
BuildTools <davidmaamoaix@gmail.com>
2021-07-21 21:27:08 +0800

    yay

--- xnlp/parser/combinator.py
+++ xnlp/parser/combinator.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
-from typing import List, Generic, TypeVar, Callable
+from typing import List, Generic, TypeVar, Callable, Union, Any
 
 #from .token import Token
 
 
 TokenT = TypeVar('TokenT')
 class Reader(Generic[TokenT]):
-    pass
+
+    def __init__(self, content: List[TokenT]):
+        self.content = content
+        self.counter = 0
 
 
 OutT = TypeVar('OutT')
@@ -16,15 +19,30 @@ class Parser(Generic[TokenT, OutT]):
     An interface for all parser combinators.
     """
 
-    def __init__(self, f: Callable[[Reader[TokenT]], OutT]):
+    def __init__(self, f: Callable[[Reader[TokenT]], List[OutT]]):
         self.func = f
 
-    def parse(self, reader: Reader[TokenT]) -> OutT:
+    def parse(self, reader: Reader[TokenT]) -> List[OutT]:
         return self.func(reader)
 
-    def __add__(self, other: Parser) -> Parser:
+    def __add__(self, other: Parser[TokenT, OutT]) -> Parser[TokenT, OutT]:
         return Join(self, other)
 
-    def __or__(self, other: Parser) -> Parser:
+    def __or__(self, other: Parser[TokenT, OutT]) -> Parser[TokenT, OutT]:
         return Or(self, other)
 
+
+class FunctorialParser(Parser[TokenT, OutT]):
+    pass
+
+
+class Join(Parser[TokenT, OutT]):
+
+    def __init__(self, a: Parser[TokenT, OutT], b: Parser[TokenT, OutT]):
+        pass
+
+
+class Or(Parser[TokenT, OutT]):
+
+    def __init__(self, a: Parser[TokenT, OutT], b: Parser[TokenT, OutT]):
+        pass
