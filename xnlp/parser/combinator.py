861e7fcbf22ad7fdf1c1dc822036c2366d9ec41d
BuildTools <davidmaamoaix@gmail.com>
2021-07-23 10:13:13 +0800

    life is bad

--- xnlp/parser/combinator.py
+++ xnlp/parser/combinator.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-from typing import List, Generic, TypeVar, Callable, Union, Any
+from typing import List, Generic, TypeVar, Callable, Union, Any, Type
 
-from .monad import Monad, Either
+from .monad import Functor, Either
 
 
 TokenT = TypeVar('TokenT')
@@ -13,47 +13,47 @@ class Reader(Generic[TokenT]):
         self.counter = 0
 
 
-SourceT = TypeVar('SourceT')
-TargetT = TypeVar('TargetT')
-class Parser(Generic[TokenT, SourceT], Monad[SourceT]):
+S = TypeVar('S')
+T = TypeVar('T')
+class Parser(Generic[TokenT, S], Functor[S]):
     """
     An interface for all parser combinators.
     """
 
-    def __init__(self, f: Callable[[Reader[TokenT]], List[SourceT]]):
+    def __init__(self, f: Callable[[Reader[TokenT]], List[S]]):
         self.func = f
 
-    def parse(self, reader: Reader[TokenT]) -> List[SourceT]:
+    def parse(self, reader: Reader[TokenT]) -> List[S]:
         return self.func(reader)
 
-    def __add__(self, p: Parser[TokenT, SourceT]) -> Parser[TokenT, SourceT]:
+    def __add__(self, p: Parser[TokenT, S]) -> Parser[TokenT, S]:
         return Join(self, p)
 
-    def __or__(self, p: Parser[TokenT, SourceT]) -> Parser[TokenT, SourceT]:
+    def __or__(self, p: Parser[TokenT, S]) -> Parser[TokenT, S]:
         return Or(self, p)
 
-    def fmap(self, f: Callable[[SourceT], TargetT]) -> Parser[TokenT, TargetT]:
+    def fmap(self, f: Callable[[S], T]) -> Parser[TokenT, T]:
         return FunctorialParser(f, self)
 
 
 MidT = TypeVar('MidT')
-class FunctorialParser(Parser[TokenT, SourceT]):
+class FunctorialParser(Parser[TokenT, S]):
 
-    def __init__(self, f: Callable[[MidT], SourceT], a: Parser[TokenT, MidT]):
+    def __init__(self, f: Callable[[MidT], S], a: Parser[TokenT, MidT]):
         self.f = f
         self.inner = a
 
-    def parse(self, reader: Reader[TokenT]) -> List[SourceT]:
+    def parse(self, reader: Reader[TokenT]) -> List[S]:
         return [self.f(i) for i in self.inner.parse(reader)]
 
 
-class Join(Parser[TokenT, SourceT]):
+class Join(Parser[TokenT, S]):
 
-    def __init__(self, a: Parser[TokenT, SourceT], b: Parser[TokenT, SourceT]):
+    def __init__(self, a: Parser[TokenT, S], b: Parser[TokenT, S]):
         pass
 
 
-class Or(Parser[TokenT, SourceT]):
+class Or(Parser[TokenT, S]):
 
-    def __init__(self, a: Parser[TokenT, SourceT], b: Parser[TokenT, SourceT]):
+    def __init__(self, a: Parser[TokenT, S], b: Parser[TokenT, S]):
         pass
--- xnlp/parser/monad.py
+++ xnlp/parser/monad.py
@@ -1,26 +1,77 @@
 from typing import List, Generic, TypeVar, Callable, Optional, Union, Type, Any
 
 
-FromT = TypeVar('FromT')
-ToT = TypeVar('ToT')
-class Functor(Generic[FromT]):
+F = TypeVar('F')
+T = TypeVar('T')
+class Functor(Generic[F]):
 
-    def fmap(self, f: Callable[[FromT], ToT]) -> Functor[ToT]:
+    def fmap(self, f: Callable[[F], T]) -> Functor[T]:
         raise NotImplementedError
 
 
-class Monad(Functor[FromT]):
+class Monad(Functor[F]):
 
     @staticmethod
-    def join(m: Monad[Monad[FromT]]) -> Monad[FromT]:
+    def join(m: Monad[Monad[F]]) -> Monad[F]:
         return m.bind(lambda x: x)
 
     @classmethod
-    def pure(cls: Type[Monad[FromT]], a: FromT) -> Monad[FromT]:
+    def pure(cls: Type[Monad[F]], a: F) -> Monad[F]:
         if cls == Monad:
             raise NotImplementedError
 
         return cls.pure(a)
 
-    def bind(self, f: Callable[[FromT], Monad[ToT]]) -> Monad[ToT]:
+    def bind(self, f: Callable[[F], Monad[T]]) -> Monad[T]:
         raise NotImplementedError
+
+
+ErrorT = TypeVar('ErrorT')
+class Either(Generic[ErrorT, F], Monad[F]):
+
+    @staticmethod
+    def fail(error: ErrorT) -> Either[ErrorT, F]:
+        return Left(error)
+
+    @classmethod
+    def pure(
+            cls: Type[Either[ErrorT, F]],
+            a: F) -> Either[ErrorT, F]:
+
+        return Right(a)
+
+
+class Left(Either[ErrorT, F]):
+
+    def __init__(self, error: ErrorT):
+        self.left = error
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Left):
+            return False
+
+        return self.left == other.left
+
+    def fmap(self, f: Callable[[F], T]) -> Either[ErrorT, T]:
+        return Either.fail(self.left)
+
+    def bind(self, f: Callable[[F], Any]) -> Either[ErrorT, T]:
+        return Either.fail(self.left)
+
+
+class Right(Either[ErrorT, F]):
+
+    def __init__(self, value: F):
+        self.right = value
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Right):
+            return False
+
+        return self.right == other.right
+
+    def fmap(self, f: Callable[[F], T]) -> Either[ErrorT, T]:
+        return Either.pure(f(self.right))
+
+    def bind(self, f: Callable[[F], Any]) -> Either[ErrorT, T]:
+        return f(self.right)
